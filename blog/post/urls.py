ee13c98204070b25a62e10853adbb98c1472478d
Raju Chaudhary <95562274+Vron07@users.noreply.github.com>
2022-09-16 13:23:41 +0530

    ADD
    Added URL for search filter

--- blog/post/urls.py
+++ blog/post/urls.py
@@ -5,7 +5,9 @@ urlpatterns = [
     path('posts/', PostList.as_view(),name='posts'),
     path('posts1/', PostList1.as_view(),name='posts1'),
     path('posts/<int:pk>/', PostDetail.as_view(),name='postdetail'),
+    path('posts/self/', PostListSelf.as_view(),name='postsearch'),
     path('comments/', CommentList.as_view(),name='comments'),
-    path('reply/', ReplyList.as_view(),name='replies'),
     path('comments/<int:pk>/', CommentDetail.as_view(),name='commentdetail'),
+    path('reply/', ReplyList.as_view(),name='replies'),
+    path('reply/self/', ReplyListSelf.as_view(),name='replysearch'),
 ]
